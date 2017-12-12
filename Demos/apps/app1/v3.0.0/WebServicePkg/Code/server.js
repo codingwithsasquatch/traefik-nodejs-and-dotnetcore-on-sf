50c87799cca77f0e80523d225be18c6907187485
Karim Kameka <kamekk@hotmail.com>
2017-12-12 13:11:16 -0500

    updated node app to show the node name

--- Demos/apps/app1/v3.0.0/WebServicePkg/Code/server.js
+++ Demos/apps/app1/v3.0.0/WebServicePkg/Code/server.js
@@ -1,15 +1,17 @@
 var http = require('http');
 
 const PORT = process.env.NODE_PORT || 3000;
-const RESPONSE = process.env.RES_STRING || "Hello World"
+const RESPONSE = process.env.Fabric_NodeName || process.env.RES_STRING;
 
 var server = http.createServer(function (req, res) {
-    var body = RESPONSE;
+    var body = '<center><h1>Test:</h1><br /><h3 style="color:green;">' + RESPONSE + '</h3></center>';
     var content_length = body.length;
     res.writeHead(200, {
         'Content-Length': content_length,
-        'Content-Type': 'text/plain' });
+        'Content-Type': 'text/html'
+    });
     res.end(body);
 });
 server.listen(PORT);
 console.log('Server is running on port ' + PORT);
+console.log('SF Node Name: ' + process.env.Fabric_NodeName);
